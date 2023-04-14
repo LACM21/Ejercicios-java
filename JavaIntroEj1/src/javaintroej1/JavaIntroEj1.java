e2a7db8a9bc99f383816cbbfd00695852c1f49bb
Luis Corniel <cornielluisalfonso15@gmail.com>
2023-04-14 12:31:39 -0400

    Primer punto de guardado

---
+++ JavaIntroEj1/src/javaintroej1/JavaIntroEj1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaintroej1;
+
+/**
+ *
+ * @author corni
+ */
+public class JavaIntroEj1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre = "Luis corniel";
+        System.out.println("Este es un cambio en rama luis");
+        System.out.println("Otro cambio");
+        // TODO code application logic here
+    }
+    
+}
---
+++ JavaIntroEj2/src/javaintroej2/JavaIntroEj2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaintroej2;
+
+/**
+ *
+ * @author corni
+ */
+public class JavaIntroEj2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int edad = 18;
+        
+        // TODO code application logic here
+    }
+    
+}
